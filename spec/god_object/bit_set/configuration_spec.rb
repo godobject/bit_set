2df93e7bcceff26c57c826caf074089ca5904836
Alexander E. Fischer <aef@raxys.net>
2013-03-07 23:25:12 +0100

    Implemented specs for Configuration.new, #== and #hash

--- spec/god_object/bit_set/configuration_spec.rb
+++ spec/god_object/bit_set/configuration_spec.rb
@@ -17,7 +17,71 @@ module GodObject
     end
 
     describe ".new" do
-      pending
+      it "should complain about an empty list" do
+        expect {
+          BitSet::Configuration.new([])
+        }.to raise_error(ArgumentError, 'At least one digit must be configured')
+      end
+
+      it "should handle a list of digit names" do
+        configuration = BitSet::Configuration.new([:first, :second, :third])
+
+        configuration.digits.should eql [:first, :second, :third]
+        configuration.enabled_character(:first).should   eql '1'
+        configuration.disabled_character(:first).should  eql '0'
+        configuration.enabled_character(:second).should  eql '1'
+        configuration.disabled_character(:second).should eql '0'
+        configuration.enabled_character(:third).should   eql '1'
+        configuration.disabled_character(:third).should  eql '0'
+      end
+
+      it "should handle a hash of digit names and their enabled representation" do
+        configuration = BitSet::Configuration.new(first: 'f', second: 's', third: 't')
+
+        configuration.digits.should eql [:first, :second, :third]
+        configuration.enabled_character(:first).should   eql 'f'
+        configuration.disabled_character(:first).should  eql '-'
+        configuration.enabled_character(:second).should  eql 's'
+        configuration.disabled_character(:second).should eql '-'
+        configuration.enabled_character(:third).should   eql 't'
+        configuration.disabled_character(:third).should  eql '-'
+      end
+
+      it "should handle a hash of digit names and both their enabled and disabled representations" do
+        configuration = BitSet::Configuration.new(first: ['f', '1'], second: ['s', '2'], third: ['t', '3'])
+
+        configuration.digits.should eql [:first, :second, :third]
+        configuration.enabled_character(:first).should   eql 'f'
+        configuration.disabled_character(:first).should  eql '1'
+        configuration.enabled_character(:second).should  eql 's'
+        configuration.disabled_character(:second).should eql '2'
+        configuration.enabled_character(:third).should   eql 't'
+        configuration.disabled_character(:third).should  eql '3'
+      end
+
+      it "should complain about multi-character enabled representations" do
+        expect {
+          BitSet::Configuration.new(first: ['f', '1'], second: ['ss', '2'], third: ['t', '3'])
+        }.to raise_error(ArgumentError, 'Invalid configuration')
+      end
+
+      it "should complain about multi-character disabled representations" do
+        expect {
+          BitSet::Configuration.new(first: ['f', '1'], second: ['s', '22'], third: ['t', '3'])
+        }.to raise_error(ArgumentError, 'Invalid configuration')
+      end
+
+      it "should complain about non String-like enabled representations" do
+        expect {
+          BitSet::Configuration.new(first: ['f', '1'], second: [2, '2'], third: ['t', '3'])
+        }.to raise_error(ArgumentError, 'Invalid configuration')
+      end
+
+      it "should complain about non String-like disabled representations" do
+        expect {
+          BitSet::Configuration.new(first: ['f', '1'], second: ['s', 2], third: ['t', '3'])
+        }.to raise_error(ArgumentError, 'Invalid configuration')
+      end
     end
 
     describe "#min" do
@@ -92,7 +156,44 @@ module GodObject
     end
 
     describe "#==" do
-      pending
+      it "should return true if digits and representation are equal" do
+        configuration = BitSet::Configuration.new(red: 'r', yellow: 'y', green: 'g')
+
+        traffic_light_configuration.should == configuration
+      end
+
+      it "should return true if only digits are equal" do
+        configuration = BitSet::Configuration.new([:red, :yellow, :green])
+
+        traffic_light_configuration.should == configuration
+      end
+
+      it "should return true if only digits are equal (different class)" do
+        configuration = OpenStruct.new
+        configuration.digits = [:red, :yellow, :green]
+
+        traffic_light_configuration.should == configuration
+      end
+
+      it "should return false if state is equal but configuration differs" do
+        pending
+        bit_set.should_not == BitSet.new(0b110, generic_configuration)
+      end
+
+      it "should return false if configuration is equal but state differs" do
+        pending
+        bit_set.should_not == BitSet.new(0b010, traffic_light_configuration)
+      end
+
+      it "should return false if both state and configuration differ" do
+        pending
+        bit_set.should_not == BitSet.new(0b010, generic_configuration)
+      end
+
+      it "should return false if compared to incompatible type" do
+        pending
+        bit_set.should_not == :incompatible
+      end
     end
 
     describe "#eql?" do
@@ -100,7 +201,21 @@ module GodObject
     end
 
     describe "#hash" do
-      pending
+      it "should be stable over multiple calls" do
+        generic_configuration.hash.should eql generic_configuration.hash
+      end
+
+      it "should differ if the digits differ" do
+        configuration = BitSet::Configuration.new([:e, :d, :c, :b, :a])
+
+        generic_configuration.hash.should_not eql configuration.hash
+      end
+
+      it "should not differ if only the representation differs" do
+        configuration = BitSet::Configuration.new([:red, :yellow, :green])
+
+        traffic_light_configuration.hash.should eql configuration.hash
+      end
     end
 
   end
