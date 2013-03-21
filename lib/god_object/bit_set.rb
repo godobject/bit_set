6f995e5216b5c38d564016751d82430487b2f434
Alexander E. Fischer <aef@raxys.net>
2013-03-21 09:08:40 +0100

    Added YARD documentation.

--- lib/god_object/bit_set.rb
+++ lib/god_object/bit_set.rb
@@ -1,19 +1,58 @@
+# encoding: UTF-8
+=begin
+Copyright Alexander E. Fischer <aef@raxys.net>, 2012-2013
+
+This file is part of BitSet.
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
+=end
+
 require 'set'
 require 'forwardable'
 
 require 'god_object/bit_set/version'
 require 'god_object/bit_set/configuration'
 
+# Namespace for projects of the GodObject team
+#
+# @see https://www.godobject.net/
 module GodObject
 
+  # A bit set with named digits and numeric internal value 
   class BitSet
+
+    # @return [Array<Symbol>] list of valid String representation formats
+    #
+    # @private
     STRING_FORMAT = Set[:long, :short].freeze
 
-    attr_reader :configuration, :integer_representation
+    # @return [GodObject::BitSet::Configuration] the configuration for the
+    #   BitSet
+    attr_reader :configuration
+
+    # @return [Integer] the BitSet as binary number
+    attr_reader :integer_representation
 
     extend Forwardable
     include Comparable
 
+    # Initializes a new BitSet
+    # 
+    # @param [Integer, Array<Symbol>] state either the octal state of the
+    #   BitSet or a list of enabled digits
+    #
+    # @param [GodObject::BitSet::Configuration] configuration the configuration
+    #   which defines the digits of the BitSet
     def initialize(state = 0, configuration)
       @configuration = Configuration.build(configuration)
 
@@ -40,6 +79,7 @@ module GodObject
       end
     end
 
+    # @return [{Symbol => true, false}] all digits and their current state
     def state
       state = {}
 
@@ -52,6 +92,15 @@ module GodObject
 
     alias attributes state
 
+    # @overload [](index)
+    #   Returns the state of a digit selected by index.
+    #   @param [Integer] index a digit index
+    #   @return [true, false] the digit's current state
+    #
+    # @overload [](digit)
+    #   Returns the state of a digit selected by name.
+    #   @param [Symbol] digit a digit name
+    #   @return [true, false] the digit's current state
     def [](index_or_digit)
       digit = find_digit(index_or_digit)
 
@@ -62,6 +111,7 @@ module GodObject
       end
     end
 
+    # @return [Array<Symbol>] a list of all digits which are enabled
     def enabled_digits
       set = Set[]
 
@@ -70,6 +120,7 @@ module GodObject
       set
     end
 
+    # @return [Array<Symbol>] a list of all digits which are disabled
     def disabled_digits
       set = Set[]
 
@@ -78,22 +129,35 @@ module GodObject
       set
     end
 
+    # @return [GodObject::BitSet] a new BitSet of the same configuration with
+    #   all digit states inverted
     def invert
       @configuration.new(@configuration.max - @integer_representation)
     end
 
+    # @param [GodObject::BitSet, Array<Symbol>] other another
+    #   BitSet
+    # @return [GodObject::BitSet] a new BitSet with the enabled
+    #   digits of the current and other
     def +(other)
       other = other.enabled_digits if other.respond_to?(:enabled_digits)
 
       @configuration.new(enabled_digits + other)
     end
 
+    # @param [GodObject::BitSet, Array<Symbol>] other another
+    #   BitSet
+    # @return [GodObject::BitSet] a new BitSet with the enabled
+    #   digits of the current without the enabled of other
     def -(other)
       other = other.enabled_digits if other.respond_to?(:enabled_digits)
 
       @configuration.new(enabled_digits - other)
     end
 
+    # @param [GodObject::BitSet, Integer] other another BitSet
+    # @return [GodObject::BitSet] a new BitSet with the enabled
+    #   digits of the current and other
     def union(other)
       other = other.to_i if other.respond_to?(:to_i)
 
@@ -102,6 +166,9 @@ module GodObject
 
     alias | union
 
+    # @param [GodObject::BitSet, Integer] other another BitSet
+    # @return [GodObject::BitSet] a new BitSet with only those
+    #   digits enabled which are enabled in both the current and other
     def intersection(other)
       other = other.to_i if other.respond_to?(:to_i)
 
@@ -110,6 +177,9 @@ module GodObject
 
     alias & intersection
 
+    # @param [GodObject::BitSet, Integer] other another BitSet
+    # @return [GodObject::BitSet] a new BitSet with only those enabled
+    #   digits which are enabled in only one of current and other
     def symmetric_difference(other)
       other = other.to_i if other.respond_to?(:to_i)
 
@@ -118,6 +188,14 @@ module GodObject
 
     alias ^ symmetric_difference
 
+    # Compares the BitSet to another to determine its relative position.
+    #
+    # BitSets are only comparable if their configuration is equal. Relative
+    # position is then defined by comparing the Integer representation.
+    #
+    # @param [GodObject::BitSet] other a BitSet
+    # @return [-1, 0, 1, nil] -1 if other is greater, 0 if other is equal and
+    #   1 if other is lesser than self, nil if comparison is impossible
     def <=>(other)
       if @configuration == other.configuration
         @integer_representation <=> other.integer_representation
@@ -128,22 +206,39 @@ module GodObject
       nil
     end
 
+    # Answers if another object is equal and of the same type family.
+    #
+    # @see GodObject::BitSet#<=>
+    # @param [Object] other an object to be checked for equality
+    # @return [true, false] true if the object is considered equal and of the
+    #   same type familiy, false otherwise
     def eql?(other)
       self == other && other.kind_of?(self.class)
     end
 
+    # @return [see Array#hash] identity hash for hash table usage
     def hash
       [@configuration, @integer_representation].hash
     end
 
+    # Represents a BitSet as String for debugging.
+    #
+    # @return [String] a String representation for debugging
     def inspect
       "#<#{self.class}: #{self.to_s.inspect}>"
     end
 
+    # Represents a BitSet as a binary Integer.
+    #
+    # @return [Integer] an Integer representation
     def to_i
       @integer_representation
     end
 
+    # Represents a BitSet as String.
+    #
+    # @param [:long, :short] format the String format
+    # @return [String] a String representation
     def to_s(format = :long)
       unless STRING_FORMAT.include?(format)
         raise ArgumentError, "Invalid format: #{format.inspect}"
@@ -173,10 +268,36 @@ module GodObject
 
     protected
 
+    # @!method digits
+    #   @attribute digits [readonly]
+    #   @return (see GodObject::BitSet::Configuration#digits)
+    #   @private
+    #
+    # @!method binary_position
+    #   @return (see GodObject::BitSet::Configuration#binary_position)
+    #   @private
+    #
+    # @!method enabled_character
+    #   @return (see GodObject::BitSet::Configuration#enabled_character)
+    #   @private
+    #
+    # @!method disabled_character
+    #   @return (see GodObject::BitSet::Configuration#disabled_character)
+    #   @private
+    #
+    # @!method find_digit
+    #   @return (see GodObject::BitSet::Configuration#find_digit)
+    #   @private
     def_delegators :@configuration,
       :digits, :binary_position, :enabled_character,
       :disabled_character, :find_digit
 
+    # For each configured digit name, a reader method and a reader method with
+    # a question mark suffix is generated to easily ask for the state of a
+    # single digit.
+    #
+    # @private
+    # @return [void]
     def create_attribute_readers
       bit_set = self
 
--- lib/god_object/bit_set/configuration.rb
+++ lib/god_object/bit_set/configuration.rb
@@ -1,117 +1,216 @@
 # encoding: UTF-8
+=begin
+Copyright Alexander E. Fischer <aef@raxys.net>, 2012-2013
+
+This file is part of BitSet.
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
+=end
 
 module GodObject
   class BitSet
 
+    # A Configuration defines the digits of a BitSet. Additionally it can hold
+    # information on how to represent the digits in a String representation.
     class Configuration
-       UNNAMED_ENABLED  = '1'.freeze
-       UNNAMED_DISABLED = '0'.freeze
-       NAMED_DISABLED   = '-'.freeze
 
-       class << self
+      # @return [String] the default String representation for enabled digits
+      UNNAMED_ENABLED  = '1'.freeze
 
-         def build(configuration)
-           if configuration.is_a?(Configuration)
-             configuration
-           else
-             new(configuration)
-           end
-         end
+      # @return [String] the default String representation for disabled digits
+      UNNAMED_DISABLED = '0'.freeze
 
-       end
+      # @return [String] the default String representation for disabled digits
+      #   which have a custom enabled representation
+      NAMED_DISABLED   = '-'.freeze
 
-       attr_reader :max
+      class << self
 
-       def initialize(configuration)
-         @digits   = {}
-         @enabled  = {}
-         @disabled = {}
+        # @overload build(configuration)
+        #   Returns an existing instance of GodObject::BitSet::Configuration.
+        #   @param [GodObject::BitSet::Configuration] configuration an already
+        #     existing Configuration
+        #   @return [GodObject::BitSet::Configuration] the same Configuration object
+        #
+        # @overload build(digits)
+        #   Returns a new Configuration object with given attributes.
+        #   @param [Array<Symbol>] digits a list of digit names
+        #   @return [GodObject::PosixMode::Mode] a new Configuration object
+        #
+        # @overload build(enabled_representations_by_digits)
+        #   Returns a new Configuration object with given attributes.
+        #   @param [Hash<Symbol => String>] enabled_representations_by_digits
+        #     digit names mapped to their enabled character representations
+        #   @return [GodObject::PosixMode::Mode] a new Configuration object
+        #
+        # @overload build(representations_by_digits)
+        #   Returns a new Configuration object with given attributes.
+        #   @param [Hash<Symbol => Array<String>>] representations_by_digits
+        #     digit names mapped to their enabled and disabled character
+        #     representations
+        #   @return [GodObject::PosixMode::Mode] a new Configuration object
+        def build(configuration)
+          if configuration.is_a?(Configuration)
+            configuration
+          else
+            new(configuration)
+          end
+        end
 
-         configuration.each do |digit, display|
-           digit = digit.to_sym
+      end
 
-           @digits[digit] = nil
+      # @return [Integer] the Integer representation if all digits are enabled
+      attr_reader :max
 
-           case
-           when display.respond_to?(:all?) && display.all?{|s| s.respond_to?(:to_str) && s.to_str.length == 1}
-             @enabled[digit]  = display.first.to_str.dup.freeze
-             @disabled[digit] = display.last.to_str.dup.freeze
-           when display.respond_to?(:to_str) && display.to_str.length == 1
-             @enabled[digit]  = display.to_str.dup.freeze
-             @disabled[digit] = NAMED_DISABLED
-           when display.nil?
-             @enabled[digit]  = UNNAMED_ENABLED
-             @disabled[digit] = UNNAMED_DISABLED
-           else
-             raise ArgumentError, 'Invalid configuration'
-           end
-         end
+      # Initializes a new BitSet::Configuration
+      # 
+      # @overload initialize(digits)
+      #   @param [Array<Symbol>] digits a list of digit names
+      #
+      # @overload initialize(enabled_representations_by_digits)
+      #   @param [Hash<Symbol => String>] enabled_representations_by_digits
+      #     digit names mapped to their enabled character representations
+      #
+      # @overload initialize(representations_by_digits)
+      #   @param [Hash<Symbol => Array<String>>] representations_by_digits
+      #     digit names mapped to their enabled and disabled character
+      #     representations
+      def initialize(configuration)
+        @digits   = {}
+        @enabled  = {}
+        @disabled = {}
 
-         raise ArgumentError, 'At least one digit must be configured' if digits.count < 1
+        configuration.each do |digit, display|
+          digit = digit.to_sym
 
-         @digits.keys.reverse.each_with_index{|digit, index| @digits[digit] = 2 ** index }
+          @digits[digit] = nil
 
-         @max = (@digits.values.first * 2) - 1
+          case
+          when display.respond_to?(:all?) && display.all?{|s| s.respond_to?(:to_str) && s.to_str.length == 1}
+            @enabled[digit]  = display.first.to_str.dup.freeze
+            @disabled[digit] = display.last.to_str.dup.freeze
+          when display.respond_to?(:to_str) && display.to_str.length == 1
+            @enabled[digit]  = display.to_str.dup.freeze
+            @disabled[digit] = NAMED_DISABLED
+          when display.nil?
+            @enabled[digit]  = UNNAMED_ENABLED
+            @disabled[digit] = UNNAMED_DISABLED
+          else
+            raise ArgumentError, 'Invalid configuration'
+          end
+        end
 
-         @unique_characters = !@enabled.values.dup.uniq!
-       end
+        raise ArgumentError, 'At least one digit must be configured' if digits.count < 1
 
-       def min
-         0
-       end
+        @digits.keys.reverse.each_with_index{|digit, index| @digits[digit] = 2 ** index }
 
-       def unique_characters?
-         @unique_characters
-       end
+        @max = (@digits.values.first * 2) - 1
 
-       def digits
-         @digits.keys
-       end
+        @unique_characters = !@enabled.values.dup.uniq!
+      end
 
-       def new(state)
-         BitSet.new(state, self)
-       end
+      # @attribute min [readonly]
+      # @return [Integer] the Integer representation if all digits are disabled
+      def min
+        0
+      end
 
-       def binary_position(digit)
-         @digits[find_digit(digit)]
-       end
+      # Answers if all digits have unique enabled representations.
+      #
+      # @return [true, false] true if all digits have unique enabled
+      #   representations, false otherwise
+      def unique_characters?
+        @unique_characters
+      end
 
-       def disabled_character(digit)
-         @disabled[find_digit(digit)]
-       end
+      # @attribute digits [readonly]
+      # @return [Array<Symbol>] an ordered list of all configured digit names
+      def digits
+        @digits.keys
+      end
 
-       def enabled_character(digit)
-         @enabled[find_digit(digit)]
-       end
+      # @param [Integer, Array<Symbol>] state either the octal state of the
+      #   BitSet or a list of enabled digits
+      # @return [GodObject::BitSet] a new BitSet object with the current
+      #   configuration
+      def new(state)
+        BitSet.new(state, self)
+      end
 
-       def find_digit(index_or_digit)
-         case
-         when index_or_digit.respond_to?(:to_sym)
-           digit = index_or_digit.to_sym
+      # @return [Integer] the Integer representation of a BitSet where
+      #   only the given digit is enabled.
+      def binary_position(digit)
+        @digits[find_digit(digit)]
+      end
 
-           raise ArgumentError, "Invalid digit name (#{index_or_digit})" unless @digits.keys.include?(digit)
-         else
-           digit = @digits.keys[index_or_digit.to_int]
-         end
+      # @param [Symbol, Integer] digit the name or index of the digit
+      # @return [String] the String representation for the given digit when
+      #   it is disabled
+      def disabled_character(digit)
+        @disabled[find_digit(digit)]
+      end
 
-         raise ArgumentError, "Invalid index or digit (#{index_or_digit})" unless digit
+      # @param [Symbol, Integer] digit the name or index of the digit
+      # @return [String] the String representation for the given digit when
+      #   it is enabled
+      def enabled_character(digit)
+        @enabled[find_digit(digit)]
+      end
 
-         digit
-       end
+      # @param [Symbol, Integer] index_or_digit the name or index of the digit
+      # @return [Symbol] the digit's name
+      def find_digit(index_or_digit)
+        case
+        when index_or_digit.respond_to?(:to_sym)
+          digit = index_or_digit.to_sym
 
-       def ==(other)
-         digits == other.digits
-       end
+          raise ArgumentError, "Invalid digit name (#{index_or_digit})" unless @digits.keys.include?(digit)
+        else
+          digit = @digits.keys[index_or_digit.to_int]
+        end
 
-       def eql?(other)
-         self == other && other.kind_of?(self.class)
-       end
+        raise ArgumentError, "Invalid index or digit (#{index_or_digit})" unless digit
 
-       def hash
-         @digits.hash
-       end
+        digit
+      end
 
-     end
+      # Answers if another object is equal.
+      #
+      # Equality is defined as having the same ordered list of digits.
+      #
+      # @param [Object] other an object to be checked for equality
+      # @return [true, false] true if the object is considered equal, false
+      #   otherwise
+      def ==(other)
+        digits == other.digits
+      end
+
+      # Answers if another object is equal and of the same type family.
+      #
+      # @see GodObject::Configuration#==
+      # @param [Object] other an object to be checked for equality
+      # @return [true, false] true if the object is considered equal and of
+      #   the same type familiy, false otherwise
+      def eql?(other)
+        self == other && other.kind_of?(self.class)
+      end
+
+      # @return (see Hash#hash) identity hash for hash table usage
+      def hash
+        @digits.hash
+      end
+
+    end
 
   end
 end
